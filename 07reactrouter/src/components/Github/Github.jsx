d741fc1e21f3d09ac71593a4297472f50012d926
AkASH jain <aj9882854@gmail.com>
2024-07-19 11:42:13 +0530

    context api

---
+++ 07reactrouter/src/components/Github/Github.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data , setData] = useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/ak123jain')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setData(data)
+    //     })
+    // }, [])
+    return (
+        <div className='text-centre m-4 bg-gray-600 text-white p-4 text-3xl' >Github follower: {data.followers} 
+        <img src={data.avatar_url} alt="" width={300} />
+        </div> 
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/ak123jain')
+    return response.json()
+}
